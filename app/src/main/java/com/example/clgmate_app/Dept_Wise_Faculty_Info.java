f5c19c786d3c5f1faa5b6f2720889f08bb525629
kunjam <raghwendrarsk7500@gmail.com>
2023-05-04 19:18:47 +0100

    new

---
+++ app/src/main/java/com/example/clgmate_app/Dept_Wise_Faculty_Info.java
@@ -0,0 +1,14 @@
+package com.example.clgmate_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dept_Wise_Faculty_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dept_wise_faculty_info);
+    }
+}
